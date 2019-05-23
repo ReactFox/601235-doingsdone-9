ccec1ef7110cd0cedcfe0b9331e870b9f0c52f13
Boleslav <itboleslav@gmail.com>
2019-05-23 21:24:49 +0300

    Решает 4-1

---
+++ schema.sql
@@ -0,0 +1,34 @@
+CREATE DATABASE doingsdone_db
+    DEFAULT CHARACTER SET utf8
+    DEFAULT COLLATE UTF8_GENERAL_CI;
+
+USE doingsdone_db;
+
+CREATE TABLE projects
+(
+    id        INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    project   TEXT(255)        NOT NULL,
+    author_id INT(11) UNSIGNED NOT NULL
+);
+
+CREATE TABLE tasks
+(
+    id                 INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    date_creation      TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    done               SMALLINT(1) UNSIGNED NOT NULL,
+    title              TEXT(255)            NOT NULL,
+    link               CHAR(128),
+    date_of_completion TIMESTAMP,
+    author_id          INT(11) UNSIGNED     NOT NULL,
+    project_id         INT(11) UNSIGNED     NOT NULL,
+    FULLTEXT (title)
+);
+
+CREATE TABLE users
+(
+    id        INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    dt_reg    TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    email     CHAR(128) NOT NULL UNIQUE,
+    user_name CHAR(64)  NOT NULL,
+    pass      CHAR(64)  NOT NULL
+);
