9fc48f37444662e4b403016255fe84d42f0c3834
Boleslav <itboleslav@gmail.com>
2019-05-25 17:34:43 +0300

    modele5 добавляет рыбу для создания запросов в базу данных

--- index.php
+++ index.php
@@ -1,10 +1,31 @@
 <?php
 require_once("helpers.php");
 require_once("data.php");
-require_once("functions.php");
 require_once ("init.php");
+require_once("functions.php");
 
-// Функция для получения кол-ва задач в категории проекты
+if (!$link) {
+    $error = mysqli_connect_error();
+    echo "<h2>$error</h2>";
+    exit;
+} else {
+    //всё ниже РЫБА TODO заполнить правильными данными
+    $sql = "SELECT dt_add, post_title, content, quote_author, image, link, show_count, p.type_id, 
+       usr_name, avatar ,class_icon FROM posts p     
+    JOIN users u ON u.id = p.author_id
+    JOIN content_type ct ON p.type_id = ct.type_id
+    ";
+    $posts = get_mysql_selection_result($link, $sql);
+
+
+    $sql_get_type_content = "SELECT * FROM content_type";
+    $type_contents = get_mysql_selection_result($link, $sql_get_type_content);
+
+    echo "<pre>";
+    print_r($type_contents);
+    echo "</pre>";
+}
+
 
 
 $content = include_template("index.php", [
